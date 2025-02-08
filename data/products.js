2e00773f5dce500513abcfbb186684d9a278d3f1
IndraMahayana <indramahayana3@gmail.com>
2025-02-08 10:45:20 +0800

    Use inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -13,24 +13,46 @@ export function getProduct(productId) {
 }
 
 class Product {
-   constructor(productDetails) {
-      this.id = productDetails.id;
-      this.image = productDetails.image;
-      this.name = productDetails.name;
-      this.rating = productDetails.rating;
-      this.priceCents = productDetails.priceCents;
-      this.keywords = productDetails.keywords;
-      this.type = productDetails.type;
-      this.sizeChartLink = productDetails.sizeChartLink;
-   }
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
 
-   getStarsUrl() {
-      return `images/ratings/rating-${this.rating.stars * 10}.png`;
-   }
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
 
-   getPrice() {
-      return `$${formatCurrency(this.priceCents)}`;
-   }
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+
+  extraInfoHTML() {
+    return ``;
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>
+    `;
+  }
 }
 
 export const products = [
@@ -693,5 +715,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
